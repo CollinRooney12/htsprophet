ce7eb3948fbd21d36f7c53159cf22f9d37e2e25b
Collin Rooney <CAR0030@u.northwestern.edu>
2017-09-15 10:42:32 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name = 'htsprophet',
-      version = '0.0.31',
+      version = '0.1.0',
       description = "Creates Hierarchical Time Series Forecasts with Facebook's Prophet tool",
       url = "https://github.com/CollinRooney12/htsprophet",
       author = "Collin Rooney",
